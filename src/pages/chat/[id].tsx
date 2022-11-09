88d5beeb7efdf04308ec24c597cb046781d1e3f1
heo-it <yejin097@naver.com>
2022-11-09 17:54:36 +0900

    feat: 채팅 불러오기

--- src/pages/chat/[id].tsx
+++ src/pages/chat/[id].tsx
@@ -1,6 +1,40 @@
 import React, { FunctionComponent } from 'react'
+import { useRouter } from "next/router";
+import Head from 'next/head';
+import SideChatBar from 'components/SideChatBar';
+import Chatroom from 'components/Chatroom';
+import { FChatItemProps } from '../../components/ChatListItem';
+import me from '../../user';
+
+import { db } from '../../firebase';
+import {
+  collection,
+  query,
+  orderBy,
+  DocumentData
+} from 'firebase/firestore';
+import { useCollection } from 'react-firebase-hooks/firestore';
 
 const ChatPage: FunctionComponent = function () {
+  const router = useRouter();
+  const id = router.query.id as string;
+
+  const getChats = (email: string, senderId: string) => {
+    /**
+       * @description 나에게 보낸 메세지 불러오는 로직
+       */
+    const q = query(collection(db, `chat/${email}/message`), orderBy("createAt"));
+    const [snapshot] = useCollection(q);
+
+    const chats = snapshot?.docs
+      .map((doc: DocumentData) => doc.data())
+      .filter(({ sender }: FChatItemProps) => sender === senderId);
+
+    return chats != null ? chats : [];
+  };
+
+  const allChats = [...getChats(me.email, id), ...getChats(id, me.email)]
+    .sort((a: FChatItemProps, b: FChatItemProps) => a.createAt.seconds - b.createAt.seconds);
 
   return (
     <>
@@ -12,7 +46,7 @@ const ChatPage: FunctionComponent = function () {
       <main>
         {/** @media 쿼리로 375px 보다 작을 경우 안보이게 하자. */}
         <SideChatBar />
-        <Chatroom />
+        <Chatroom id={id} chats={allChats} />
       </main>
     </>
   )
