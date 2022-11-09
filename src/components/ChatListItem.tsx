7e2f906506b950fa680f69b31ae5ddea092c2a52
heo-it <yejin097@naver.com>
2022-11-09 17:59:02 +0900

    feat: 채팅방 리스트 클릭시 채팅방 페이지 이동

--- src/components/ChatListItem.tsx
+++ src/components/ChatListItem.tsx
@@ -1,5 +1,6 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, MouseEvent } from 'react';
 import styles from '../styles/chatListItem.module.css';
+import { useRouter } from 'next/router';
 import { BiUserCircle } from 'react-icons/bi';
 interface ChatItem {
   id: string,
@@ -20,17 +21,26 @@ const ChatListItem: FunctionComponent<ChatListItemProps> = ({
   name,
   messages
 }: ChatListItemProps) => {
+  const router = useRouter();
+
+  const handleChatClick = (e: MouseEvent) => {
+    e.preventDefault();
+    router.push(`/chat/${name}`);
+  };
+
   return (
+    <a onClick={handleChatClick}>
     <div className={styles.container}>
-      <BiUserCircle color="gray" size={60} />
-      <div className={styles.textContainer}>
-        <div className={styles.information}>
-          <span className={styles.name}>{name}</span>
-          <span className={styles.date}>• {messages.length > 0 ? messages[0].createAt.seconds : ''}</span>
+        <BiUserCircle color="gray" size={60} />
+        <div className={styles.textContainer}>
+          <div className={styles.information}>
+            <span className={styles.name}>{name}</span>
+            <span className={styles.date}>• {messages.length > 0 ? messages[0].createAt.date : ''}</span>
+          </div>
+          <p className={styles.preview}>{messages.length > 0 ? messages[0].message : ''}</p>
         </div>
-        <p className={styles.preview}>{messages.length > 0 ? messages[0].message : ''}</p>
-      </div>
     </div>
+    </a>
   )
 }
 
